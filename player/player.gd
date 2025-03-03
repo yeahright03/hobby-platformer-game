3c920f08ea97187d04ca84d64e10206741c5277e
Martin P.J <martinjansander@gmail.com>
2025-03-03 17:00:28 +0100

    added oldm tutorial asset
    added another asset pack which has a few sound effects

--- player/player.gd
+++ player/player.gd
@@ -17,29 +17,29 @@ func _physics_process(delta: float) -> void:
 			extra_jumps = 1
 			t += delta * 0.8
 			velocity.y = 50 + (150 - 50) * t
-			if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left"):
+			if Input.is_action_pressed("move-right") or Input.is_action_pressed("move-left"):
 				anim.play("wall-grab-look")
 			else:
 				anim.play("wall-grab")
 
 # jump only available when pressing direction keys
-			if Input.is_action_just_pressed("ui_accept") and (Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")):
-				if Input.is_action_pressed("ui_right"):
+			if Input.is_action_just_pressed("jump") and (Input.is_action_pressed("move-right") or Input.is_action_pressed("move-left")):
+				if Input.is_action_pressed("move-right"):
 					velocity.x = SPEED
-				elif Input.is_action_pressed("ui_left"):
+				elif Input.is_action_pressed("move-left"):
 					velocity.x = -SPEED
 		else:
 			velocity += get_gravity() * delta
 			t = 0.0
 
 # handles jumps from floor
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("jump")
 
 # handles jumps in air or from wall
-	elif Input.is_action_just_pressed("ui_accept") and not is_on_floor():
-		if not (Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")) and is_on_wall():
+	elif Input.is_action_just_pressed("jump") and not is_on_floor():
+		if not (Input.is_action_pressed("move-right") or Input.is_action_pressed("move-left")) and is_on_wall():
 			pass
 # if the extra jumps exceed zero then the function may proceed
 		elif extra_jumps > 0:
@@ -56,7 +56,7 @@ func _physics_process(delta: float) -> void:
 
 # Get the input direction and handle the movement/deceleration.
 # As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move-left", "move-right")
 	if is_on_wall():
 		pass
 	elif direction == -1:
